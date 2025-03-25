dfe82bfed38125c39ac9b245a623dddc93ae08b9
sergey <sgkopytow@yandex.ru>
2025-03-25 19:58:32 +0300

    add verification of the jwt token

--- server/main.py
+++ server/main.py
@@ -8,19 +8,12 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.include_router(admin_router, prefix="/admin")
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  
+    allow_headers=["*"], 
 )
 
 load_dotenv("./.env")
