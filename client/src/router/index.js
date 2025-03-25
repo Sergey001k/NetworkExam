2eb7d260d3d3c928656c568c6dc50b60ac024855
Svyat <svag7511@gmail.com>
2025-03-25 21:41:23 +0300

    add jwt check and admin panel

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,23 +1,69 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AdminView from '../views/AdminView.vue'
+import LoginView from '../views/LoginView.vue'
+
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/admin',
+      name: 'admin',
+      component: AdminView,
+      meta: {
+        requiresAuth: true,
+        title: 'Админ-панель'
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+      meta: {
+        title: 'Авторизация',
+        hideForAuth: true
+      }
     },
-  ],
-})
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/login'
+    }
+  ]
+});
+
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('authToken')
+  const isAuthPage = to.matched.some(record => record.meta.hideForAuth)
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+
+
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
+
+
+  if (isAuthPage && token) {
+    return next('/admin');
+  }
+
+
+  if (requiresAuth && !token) {
+    return next('/login');
+  }
+
+
+  if (!token && !isAuthPage && to.path !== '/login') {
+    return next('/login');
+  }
+
+
+  next();
+});
 
 export default router
